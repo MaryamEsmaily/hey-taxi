8d7bd16cb31d52fea4c0c1e95373a2230cd2bbd7
Maryam Esmaili <maryam.esmaily78@gmail.com>
2022-07-24 00:48:48 +0430

    rtl project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "styles/theme/theme";
+import { CacheProvider } from "@emotion/react";
+import cacheRtl from "utility/cacheRtlEmotion";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <CacheProvider value={cacheRtl}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
