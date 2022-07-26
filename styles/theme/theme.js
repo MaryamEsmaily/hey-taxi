dbe91aaa8bd21d7c104ed444ea78c368d2889ad8
Maryam Esmaili <maryam.esmaily78@gmail.com>
2022-07-26 21:04:54 +0430

    add theme

--- styles/theme/theme.js
+++ styles/theme/theme.js
@@ -1,7 +1,64 @@
-import { createTheme } from "@mui/material/styles";
+import { createTheme } from "@mui/material";
 
 const theme = createTheme({
   direction: "rtl",
+  typography: {
+    fontFamily: [
+      "IRYekan",
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+  },
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#2658cc",
+    },
+    background: {
+      default: "#e3e5ea",
+      paper: "#1d1e22",
+      over: "#2c2d31",
+    },
+    neutral: {
+      main: "#ffc73f",
+      contrastText: "#fff",
+    },
+  },
+  components: {
+    MuiTextField: {
+      defaultProps: {
+        margin: "dense",
+        fullWidth: true,
+      },
+    },
+    MuiFormControl: {
+      defaultProps: {
+        margin: "dense",
+        fullWidth: true,
+      },
+    },
+    MuiButton: {
+      defaultProps: {
+        variant: "contained",
+        disableElevation: true,
+      },
+    },
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          backgroundImage: "unset",
+        },
+      },
+    },
+  },
 });
 
 export default theme;
