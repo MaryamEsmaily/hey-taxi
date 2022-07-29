2290aecf7c960795ae9464455fc1ac319f8b2f1b
Maryam Esmaili <maryam.esmaily78@gmail.com>
2022-07-29 18:08:46 +0430

    fix map

--- components/Map/index.jsx
+++ components/Map/index.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 function Map() {
   return (
-    <MapContainer center={[35.7219, 51.3347]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer
+      style={{ height: "100vh" }}
+      center={[35.7219, 51.3347]}
+      zoom={13}
+      scrollWheelZoom={false}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
