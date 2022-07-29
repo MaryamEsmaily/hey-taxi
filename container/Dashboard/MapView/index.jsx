c19c731cabccafc6f2dd6be8da72942fe08a0008
Maryam Esmaili <maryam.esmaily78@gmail.com>
2022-07-29 16:40:16 +0430

    update mapView

--- container/Dashboard/MapView/index.jsx
+++ container/Dashboard/MapView/index.jsx
@@ -1,7 +1,14 @@
+import { Box } from "@mui/material";
+import dynamic from "next/dynamic";
 import React from "react";
 
+const Map = dynamic(() => import("components/Map"), { ssr: false });
 function MapView() {
-  return <div>MapView</div>;
+  return (
+    <Box overflow="hidden" height="100vh">
+      <Map />
+    </Box>
+  );
 }
 
 export default MapView;
