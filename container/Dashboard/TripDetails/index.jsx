9cc66772c6f0c707e6feb61eea2404bc3bfd8fff
Maryam Esmaili <maryam.esmaily78@gmail.com>
2022-07-29 16:39:15 +0430

    update TripDetails

--- container/Dashboard/TripDetails/index.jsx
+++ container/Dashboard/TripDetails/index.jsx
@@ -1,49 +1,151 @@
-import { Box, InputAdornment, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Divider,
+  InputAdornment,
+  TextField,
+  Typography,
+  Button,
+  IconButton,
+} from "@mui/material";
 import React from "react";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import PlaceIcon from "@mui/icons-material/Place";
-import HomeIcon from "@mui/icons-material/Home";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import PlaceOutlinedIcon from "@mui/icons-material/PlaceOutlined";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import CircleIcon from "@mui/icons-material/Circle";
+import SquareIcon from "@mui/icons-material/Square";
+import LocalTaxiIcon from "@mui/icons-material/LocalTaxi";
+import PeopleAltOutlinedIcon from "@mui/icons-material/PeopleAltOutlined";
 //
 function TripDetails() {
   return (
-    <Box p={4} bgcolor="white">
+    <Box pt={5} px={6} sx={{ bgcolor: "background.paper" }} height="100%">
       <Box
+        mb={8}
         sx={{
           display: "flex",
           alignItems: "center",
           justifyContent: "space-between",
         }}
       >
-        <AccountCircleIcon />
-        <Typography>هی تاکسی!</Typography>
+        <IconButton>
+          <AccountCircleOutlinedIcon color="warning" />
+        </IconButton>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <LocalTaxiIcon color="warning" sx={{ fontSize: 20 }} />
+          <Typography>هی تاکسی!</Typography>
+        </Box>
       </Box>
+
+      <Typography
+        fontSize="24px"
+        sx={{
+          textAlign: "center",
+        }}
+        mb={3}
+      >
+        مشخصات مسیر
+      </Typography>
       <Box
-        mt={5}
+        mb={8}
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-around",
+        }}
+      >
+        <Box px={2}>
+          <TextField
+            sx={{ mb: 2 }}
+            placeholder="مبدا"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <HomeOutlinedIcon color="warning" />
+                </InputAdornment>
+              ),
+            }}
+          />
+          <TextField
+            placeholder="مقصد"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <PlaceOutlinedIcon color="warning" />
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <CircleIcon color="warning" sx={{ fontSize: 8 }} />
+          <Divider
+            orientation="vertical"
+            sx={{ height: 45, borderColor: "#ffc73f" }}
+            variant="middle"
+          />
+          <SquareIcon color="warning" sx={{ fontSize: 8 }} />
+        </Box>
+      </Box>
+      <Typography
+        fontSize="24px"
+        my={3}
         sx={{
           textAlign: "center",
         }}
       >
-        <Typography fontSize="24px">مشخصات مسیر</Typography>
-        <TextField
-          placeholder="مبدا"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <HomeIcon />
-              </InputAdornment>
-            ),
-          }}
-        />
-        <TextField
-          placeholder="مقصد"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <PlaceIcon />
-              </InputAdornment>
-            ),
-          }}
-        />
+        تعداد نفرات
+      </Typography>
+      <TextField
+        type="number"
+        defaultValue={1}
+        InputProps={{
+          inputProps: { min: 1, max: 3 },
+          startAdornment: (
+            <InputAdornment position="start">
+              <PeopleAltOutlinedIcon color="warning" />
+            </InputAdornment>
+          ),
+        }}
+        sx={{
+          pl: 2,
+          pr: 3,
+          mb: 10,
+          textAlign: "center",
+          "*::-webkit-inner-spin-button": {
+            opacity: 1,
+          },
+          "*::-webkit-outer-spin-button": {
+            opacity: 1,
+          },
+          "& input": {
+            textAlign: "center",
+          },
+        }}
+      />
+      <Box pl={2} pr={3}>
+        <Button
+          type="submit"
+          fullWidth
+          sx={{ borderRadius: "50px" }}
+          color="neutral"
+        >
+          ثبت درخواست
+        </Button>
+        <Button
+          fullWidth
+          sx={{ borderRadius: "50px", mt: 1 }}
+          color="neutral"
+          variant="text"
+        >
+          لغو
+        </Button>
       </Box>
     </Box>
   );
