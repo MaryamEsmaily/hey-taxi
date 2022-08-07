ea3fb0eedeb35b788d178cbd895defdb0e32ea05
Maryam Esmaili <maryam.esmaily78@gmail.com>
2022-08-07 14:35:27 +0430

    edit map

--- components/Map/RoutingMachine.jsx
+++ components/Map/RoutingMachine.jsx
@@ -2,58 +2,31 @@ import { useEffect, useState } from "react";
 import L from "leaflet";
 import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
 import "leaflet-routing-machine";
-import { useMap, useMapEvents } from "react-leaflet";
+import { useMap } from "react-leaflet";
 
-L.Marker.prototype.options.icon = L.icon({
-  iconSize: [25, 41],
-  iconAnchor: [10, 41],
-  popupAnchor: [2, -40],
-  iconUrl: "https://unpkg.com/leaflet@1.7/dist/images/marker-icon.png",
-  shadowUrl: "https://unpkg.com/leaflet@1.7/dist/images/marker-shadow.png",
-});
-
-const RoutingMachine = ({
-  sourceCity,
-  destinationCity,
-  setOrigin,
-  setDestination,
-}) => {
+const RoutingMachine = ({ markers }) => {
   const map = useMap();
-  const [data, setData] = useState([]);
+  const [waypoints, setWaypoints] = useState();
 
-  useMapEvents({
-    click: (e) => {
-      const { lat, lng } = e.latlng;
-      if (data.length < 2) {
-        setData([...data, [lat, lng]]);
-        if (data.length < 1) {
-          setOrigin([lat, lng]);
-        }
-        setDestination([lat, lng]);
-      }
-    },
-  });
+  useEffect(() => {
+    setWaypoints(markers);
+  }, [markers]);
 
   useEffect(() => {
     if (!map) return;
-
     const routingControl = L.Routing.control({
-      waypoints: [
-        L.latLng(sourceCity?.[0], sourceCity?.[1]),
-        L.latLng(destinationCity?.[0], destinationCity?.[1]),
-      ],
-      routeWhileDragging: true,
+      waypoints,
       lineOptions: {
-        styles: [{ color: "#6FA1EC", weight: 4 }],
+        styles: [{ color: "#fff200", weight: 5 }],
       },
-      show: true,
-      showAlternatives: true,
+      show: false,
+      showAlternatives: false,
       addWaypoints: true,
-      fitSelectedRoutes: true,
+      fitSelectedRoutes: false,
+      draggableWaypoints: true,
     }).addTo(map);
-
     return () => map?.removeControl(routingControl);
-  }, [map, sourceCity, destinationCity]);
+  }, [map, waypoints]);
 
   return null;
 };
--- components/Map/index.jsx
+++ components/Map/index.jsx
@@ -1,9 +1,51 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { MapContainer, TileLayer, useMapEvents } from "react-leaflet";
+
+import Fab from "@mui/material/Fab";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ClearIcon from "@mui/icons-material/Clear";
 import RoutingMachine from "./RoutingMachine";
 
-const Map = ({ setOrigin, setDestination, destination, origin }) => {
+function Events({ onClick }) {
+  const map = useMapEvents({
+    click: (e) => {
+      onClick(e);
+    },
+  });
+  return null;
+}
+
+const MyMap = () => {
   const [map, setMap] = useState(null);
+  const [status, setStatus] = useState(0);
+  const [markers, setMarkers] = useState([]);
+  //
+  const handleClick = (location) => {
+    if (status === 0) {
+      setStatus(1);
+      setMarkers([
+        {
+          ...location.latlng,
+          type: 1,
+        },
+      ]);
+      return;
+    }
+    if (status === 1) {
+      setStatus(2);
+      setMarkers((prev) => {
+        return [
+          ...prev,
+          {
+            ...location.latlng,
+            type: 2,
+          },
+        ];
+      });
+      return;
+    }
+  };
+  //
 
   const routingMachineRef = useRef();
   const pluginRef = useRef();
@@ -13,29 +55,46 @@ const Map = ({ setOrigin, setDestination, destination, origin }) => {
     const controlContainer = routingMachineRef.current.onAdd(map);
     pluginRef.current.appendChild(controlContainer);
   }, [map]);
-
+  //
   return (
-    <MapContainer
-      style={{ height: "100vh" }}
-      center={[35.7219, 51.3347]}
-      zoom={15}
-      scrollWheelZoom={false}
-      whenCreated={setMap}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <RoutingMachine
-        destinationCity={destination}
-        sourceCity={origin}
-        setOrigin={setOrigin}
-        setDestination={setDestination}
-        ref={routingMachineRef}
-        map={map}
-      />
-    </MapContainer>
+    <>
+      {markers.length ? (
+        <Fab
+          color="warning"
+          sx={{
+            position: "fixed",
+            left: "10px",
+            top: "10px",
+            zIndex: 99999999999,
+          }}
+          onClick={() => {
+            setMarkers((prev) => {
+              const newArr = JSON.parse(JSON.stringify(prev));
+              setStatus(newArr.pop().type - 1);
+              return newArr;
+            });
+          }}
+        >
+          {markers.length === 1 ? <ClearIcon /> : <ArrowBackIcon />}
+        </Fab>
+      ) : null}
+
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{ height: "100vh" }}
+        whenCreated={setMap}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Events onClick={handleClick} />
+        <RoutingMachine markers={markers} ref={routingMachineRef} map={map} />
+      </MapContainer>
+    </>
   );
 };
 
-export default Map;
+export default MyMap;
