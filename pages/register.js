28305d891f9f674e24d06759d12837d2f9bc07d4
Maryam Esmaili <maryam.esmaily78@gmail.com>
2022-08-21 13:53:44 +0430

    change register

--- pages/register.js
+++ pages/register.js
@@ -61,117 +61,110 @@ function RegisterPage() {
   });
 
   return (
-    <Box borderRadius="8px" width="100%" maxWidth={1000} overflow="hidden">
-      <Grid container height="700px">
-        <Grid item xs={6}>
-          <Box
-            backgroundColor="#ffc73f"
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: " center",
-            }}
-            // borderRadius="4px"
-            height="100%"
-          >
-            <Box
-              borderRadius="8px"
-              backgroundColor="white"
-              mx={8}
-              p={5}
-              component="form"
-              onSubmit={formik.handleSubmit}
-            >
-              <Typography fontSize="18px" fontWeight="bold" mb={1}>
-                ثبت نام
-              </Typography>
-              <Typography fontSize="12px" fontWeight="bold" color="gray" mb={1}>
-                مشخصات خود را وارد کنید
-              </Typography>
+    <Box borderRadius="20px" width="100%" maxWidth={470} overflow="hidden">
+      <Box
+        py={3}
+        backgroundColor="#ffc73f"
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: " center",
+        }}
+        height="100%"
+      >
+        <Box
+          borderRadius="10px"
+          backgroundColor="white"
+          mx={4}
+          p={4}
+          component="form"
+          onSubmit={formik.handleSubmit}
+        >
+          <Typography fontSize="18px" fontWeight="bold">
+            ثبت نام
+          </Typography>
+          <Typography fontSize="12px" fontWeight="bold" color="gray" mb={1}>
+            مشخصات خود را وارد کنید
+          </Typography>
+          <TextField
+            label="نام کاربری"
+            {...formik.getFieldProps("username")}
+            {...getValidationFieldProps(formik, "username")}
+          />
+          <TextField
+            label="شماره موبایل"
+            {...formik.getFieldProps("phoneNo")}
+            {...getValidationFieldProps(formik, "phoneNo")}
+          />
+          <PasswordInput
+            label="رمز عبور"
+            {...formik.getFieldProps("password")}
+            {...getValidationFieldProps(formik, "password")}
+          />
+          <PasswordInput
+            label="تکرار رمز عبور"
+            {...formik.getFieldProps("rePassword")}
+            {...getValidationFieldProps(formik, "rePassword")}
+          />
+          <FormControl size="small">
+            <InputLabel>نقش کاربر</InputLabel>
+            <Select label="نقش کاربر" {...formik.getFieldProps("role")}>
+              <MenuItem value={1}>مسافر</MenuItem>
+              <MenuItem value={2}>راننده</MenuItem>
+            </Select>
+          </FormControl>
+          {formik.values.role === 2 ? (
+            <>
               <TextField
-                label="نام کاربری"
-                {...formik.getFieldProps("username")}
-                {...getValidationFieldProps(formik, "username")}
+                label="مدل اتومبیل"
+                {...formik.getFieldProps("car")}
+                {...getValidationFieldProps(formik, "car")}
               />
               <TextField
-                label="شماره موبایل"
-                {...formik.getFieldProps("phoneNo")}
-                {...getValidationFieldProps(formik, "phoneNo")}
+                label="پلاک اتومبیل"
+                {...formik.getFieldProps("carId")}
+                {...getValidationFieldProps(formik, "carId")}
               />
-              <PasswordInput
-                label="رمز عبور"
-                {...formik.getFieldProps("password")}
-                {...getValidationFieldProps(formik, "password")}
+            </>
+          ) : null}
+          <FormControl>
+            <FormLabel sx={{ display: "unset" }}>جنسیت</FormLabel>
+            <RadioGroup row {...formik.getFieldProps("gender")}>
+              <FormControlLabel
+                value="2"
+                control={<Radio size="small" color="warning" />}
+                label="زن"
               />
-              <PasswordInput
-                label="تکرار رمز عبور"
-                {...formik.getFieldProps("rePassword")}
-                {...getValidationFieldProps(formik, "rePassword")}
+              <FormControlLabel
+                value="1"
+                control={<Radio size="small" color="warning" />}
+                label="مرد"
               />
-              <FormControl size="small">
-                <InputLabel>نقش کاربر</InputLabel>
-                <Select label="نقش کاربر" {...formik.getFieldProps("role")}>
-                  <MenuItem value={1}>مسافر</MenuItem>
-                  <MenuItem value={2}>راننده</MenuItem>
-                </Select>
-              </FormControl>
-              {formik.values.role === 2 ? (
-                <>
-                  <TextField
-                    label="مدل اتومبیل"
-                    {...formik.getFieldProps("car")}
-                    {...getValidationFieldProps(formik, "car")}
-                  />
-                  <TextField
-                    label="پلاک اتومبیل"
-                    {...formik.getFieldProps("carId")}
-                    {...getValidationFieldProps(formik, "carId")}
-                  />
-                </>
-              ) : null}
-              <FormControl>
-                <FormLabel sx={{ display: "unset" }}>جنسیت</FormLabel>
-                <RadioGroup row {...formik.getFieldProps("gender")}>
-                  <FormControlLabel
-                    value="2"
-                    control={<Radio size="small" color="warning" />}
-                    label="زن"
-                  />
-                  <FormControlLabel
-                    value="1"
-                    control={<Radio size="small" color="warning" />}
-                    label="مرد"
-                  />
-                  <FormControlLabel
-                    value="3"
-                    control={<Radio size="small" color="warning" />}
-                    label="سایر"
-                  />
-                </RadioGroup>
-              </FormControl>
+              <FormControlLabel
+                value="3"
+                control={<Radio size="small" color="warning" />}
+                label="سایر"
+              />
+            </RadioGroup>
+          </FormControl>
 
-              <Button
-                type="submit"
-                fullWidth
-                sx={{ borderRadius: "50px", mt: 2 }}
-                color="neutral"
-              >
-                ثبت نام
-              </Button>
-              <Box sx={{ textAlign: "center" }}>
-                <Link href="/login" passHref>
-                  <Typography component="a" fontSize="12px">
-                    حساب کاربری دارید؟ ورود
-                  </Typography>
-                </Link>
-              </Box>
-            </Box>
+          <Button
+            type="submit"
+            fullWidth
+            sx={{ borderRadius: "50px", mt: 2 }}
+            color="neutral"
+          >
+            ثبت نام
+          </Button>
+          <Box sx={{ textAlign: "center" }}>
+            <Link href="/login" passHref>
+              <Typography component="a" fontSize="12px">
+                حساب کاربری دارید؟ ورود
+              </Typography>
+            </Link>
           </Box>
-        </Grid>
-        <Grid item xs={6}>
-          <RegisterPlacard />
-        </Grid>
-      </Grid>
+        </Box>
+      </Box>
     </Box>
   );
 }
