4bf5c9001be7721e23445f874126a94a8f48cf11
Maryam Esmaili <maryam.esmaily78@gmail.com>
2022-08-28 21:20:38 +0430

    add landing

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,44 @@
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import Menu from "container/home/Menu";
+import Image from "next/image";
+import { useRouter } from "next/router";
+
 export default function Home() {
-  return <div></div>;
+  const { push } = useRouter();
+  return (
+    <Box
+      sx={{
+        width: "100vw",
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Stack spacing={10} alignItems="center" mt={5}>
+        <Box>
+          <Typography fontSize="58px" fontWeight="bold" color="#ffc73f">
+            هی تاکسی
+          </Typography>
+          <Typography fontSize="30px" fontWeight="bold" color="#ffc73f">
+            سرویس آنلاین مسافربری اقتصادی
+          </Typography>
+        </Box>
+        <Stack
+          direction="row"
+          alignItems="center"
+          fontSize="20px"
+          fontWeight="bold"
+          spacing={10}
+        >
+          <Typography fontSize="30px" color="#727272">
+            راحت سفر کنید
+          </Typography>
+          <Button onClick={() => push("/login")} borderRadius="lg" px={8}>
+            ورود
+          </Button>
+        </Stack>
+      </Stack>
+    </Box>
+  );
 }
